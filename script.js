4a9e1cc904173bbb7c322156d3ae8c87ac6b9365
Tinashe Andre <tamakwiramiti10@gmail.com>
2025-05-26 19:34:13 +0200

    Major site update

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed("#typed", {
+    strings: ["Web Developer?", "Graphic Designer?", "Mobile App Developer?"],
+    typeSpeed: 45,
+    backSpeed: 45,
+    backDelay: 1500,
+    loop: true
+  });
+  
